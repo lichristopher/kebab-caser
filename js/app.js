49d32ce5a99d682b180cde0ae54e371375ef33c1
Christopher Li <li.christopher.jr@gmail.com>
2022-07-06 13:26:42 +0800

    remove button and event listener

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
 const textArea = document.querySelector('textarea');
-const button = document.querySelector('button');
 const kebabTextDisplay = document.querySelector('#kebab-case-display');
 
-button.addEventListener('click', function (e) {
-  kebabTextDisplay.textContent = textArea.value
-    .toLowerCase()
-    .split(' ')
-    .join('-');
+textArea.addEventListener('input', function (e) {
+  kebabTextDisplay.textContent =
+    textArea.value.toLowerCase().split(' ').join('-') + '.js';
 });
